bdc78fe3dbb2811e75a207ddd6e840a94e2b1dd5
Nohoarii FAMIBELLE <famibellenohoarii@hotmail.com>
2025-11-25 21:48:45 +0100

    fix(backend) : Fix entryFile in NextJS nest-cli.js file

---
+++ database/seeds/users.seed.d.ts
@@ -0,0 +1,7 @@
+export declare const userSeeds: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    isActive: boolean;
+}[];
---
+++ database/seeds/users.seed.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userSeeds = void 0;
+exports.userSeeds = [
+    {
+        email: "admin@secondlife.com",
+        password: "$2b$10$YQ98PzLBKGqGk8J1H/ZQxOXKZz/rRvYPYZ1QGnPxPl0QxZ5QxZ5Qx",
+        firstName: "Admin",
+        lastName: "Second Life",
+        isActive: true,
+    },
+    {
+        email: "john.doe@example.com",
+        password: "$2b$10$N9qo8uLOickgx2ZMRZoMye8P6S8VqcF4z4gT9kZqJz0qxZ5QxZ5Qx",
+        firstName: "John",
+        lastName: "Doe",
+        isActive: true,
+    },
+    {
+        email: "jane.smith@example.com",
+        password: "$2b$10$N9qo8uLOickgx2ZMRZoMye8P6S8VqcF4z4gT9kZqJz0qxZ5QxZ5Qx",
+        firstName: "Jane",
+        lastName: "Smith",
+        isActive: true,
+    },
+    {
+        email: "alice.martin@example.com",
+        password: "$2b$10$N9qo8uLOickgx2ZMRZoMye8P6S8VqcF4z4gT9kZqJz0qxZ5QxZ5Qx",
+        firstName: "Alice",
+        lastName: "Martin",
+        isActive: true,
+    },
+    {
+        email: "bob.wilson@example.com",
+        password: "$2b$10$N9qo8uLOickgx2ZMRZoMye8P6S8VqcF4z4gT9kZqJz0qxZ5QxZ5Qx",
+        firstName: "Bob",
+        lastName: "Wilson",
+        isActive: true,
+    },
+];
+//# sourceMappingURL=users.seed.js.map
